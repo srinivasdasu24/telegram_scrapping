7572f5fe1779f6fab5154ee6e8972e07bea968ea
srinivas dasu <srinivas.dasucse@gmail.com>
2020-05-31 19:03:36 +0530

    Updated comments

--- telegram_chat_count.py
+++ telegram_chat_count.py
@@ -5,17 +5,17 @@ from telethon import TelegramClient,sync
 from telethon.errors import SessionPasswordNeededError
 
 # (1) Use your own values here
-api_id = 17349
-api_hash = '344583e45741c457fe1862106095a5eb'
+api_id = api_id # eg: 17349
+api_hash = 'api_hash' #eg :'344583e45741c457fe1862106095a5eb'
 
-phone = '919603594976'
-username = 'dasu24'
+phone = 'your_phone_number' # '919603594976'
+username = 'your_user_name' #eg 'dasu24'
 
 # (2) Create the client and connect
 client = TelegramClient(username, api_id, api_hash)
 client.connect()
 
-# Ensure you're authorized
+# (3) Ensure you're authorized
 if not client.is_user_authorized():
     client.send_code_request(phone)
     try:
@@ -26,7 +26,7 @@ if not client.is_user_authorized():
 me = client.get_me()
 print(me)
 
-#Get the message count from all groups where 'user_name' is a member and individaul chats 
+# (4) Get the message count from all groups where 'user_name' is a member and individaul chats 
 entities = client.get_dialogs(limit=30)
 
 counts = []
@@ -38,7 +38,7 @@ for e in entities:
 
     count= client.get_messages(e, limit=1)
     counts.append((name, count.total))
-
+# (5) Sort the group name and count based on count 
 counts.sort(key=lambda x: x[1], reverse=True)
 for name, count in counts:
     print('{}: {}'.format(name, count))
